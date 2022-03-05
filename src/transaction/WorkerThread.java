3a0775d3f004482e29125d509ad8cb67eed5b3fc
Chau Ho <chauho@iPhonecaVietAnh.localdomain>
2022-03-05 16:37:45 +0700

    Add function send response and receive client resquest

--- src/transaction/WorkerThread.java
+++ src/transaction/WorkerThread.java
@@ -1,11 +1,11 @@
 package transaction;
 
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import app.user.UserConcreteComponent;
+
+import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class WorkerThread extends Thread {
     private Socket socket;
@@ -14,28 +14,53 @@ public class WorkerThread extends Thread {
         this.socket = socket;
     }
 
-    private String handleClientRequest(String request) {
-        return "Success";
-    }
-
-    public void processData() {
-        System.out.println("Processing: " + socket);
+    private String receiveClientRequest(Socket socket) {
         try {
             DataInputStream dataIn = new DataInputStream(socket.getInputStream());
 
             int length = dataIn.readInt();  // read length of incoming message
-            if(length > 0) {
+            if (length > 0) {
                 byte[] buffer = new byte[length];
                 dataIn.readFully(buffer, 0, buffer.length); // read the message
 
                 String requestData = new String(buffer);
-
-                //process request from client: tra cuu so du, thanh toan,...
-                String response = handleClientRequest(requestData);
+                return requestData;
             }
+            return null;
         } catch (IOException e) {
-            System.err.println("Request Processing Error: " + e);
+            e.printStackTrace();
+            return null;
         }
+    }
+
+    private String handleClientRequest(String request) {
+        return "Success";
+    }
+
+    private void sendResponse(Socket socket, String response) {
+        try {
+            byte[] serverResponse = response.getBytes();
+
+            DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream());
+
+            dataOut.writeInt(serverResponse.length); // write length of the message
+            dataOut.write(serverResponse);
+        } catch (IOException e) {}
+    }
+
+    //Handle client request and send response to client
+    public void run() {
+        System.out.println("Processing: " + socket);
+
+        //Receive request from client
+        String requestData = receiveClientRequest(socket);
+        System.out.println(requestData);
+
+        //Process request from client: tra cuu so du, thanh toan,...
+        String response = handleClientRequest(requestData);
+
+        //Send response to client
+        sendResponse(socket, response);
         System.out.println("Complete processing: " + socket);
     }
 }
