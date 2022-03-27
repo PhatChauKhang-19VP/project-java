fd5609ad9bc63e3e47bda63eec4f79a277a46b63
phatnm-student-hcmus <19126008@student.hcmus.edu.vn>
2022-03-27 14:49:45 +0700

    Update client socket communication, add encrypt and decrypt data

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,31 +1,43 @@
 package app;
 
 import app.database.DatabaseCommunication;
-import app.product.Order;
-import app.product.Package;
-import app.product.Product;
 import app.user.*;
-
-import java.sql.*;
-import java.time.LocalDateTime;
-import java.util.*;
-
-import app.util.*;
-import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         App app = App.getInstance();
+        DatabaseCommunication dbCom = DatabaseCommunication.getInstance();
         IUser admin = Admin.getInstance();
+
+        // test login
+        System.out.println(dbCom.login("admin1", "1"));
+
         // get manager
-        DatabaseCommunication.getInstance().loadAdmin();
+        dbCom.loadAdmin();
 
         // load administrativeDivision
-        DatabaseCommunication.getInstance().loadAdministrativeDivision();
+        dbCom.loadAdministrativeDivisions();
 
-        for (String key : app.getUserList().keySet()){
-            app.getUserList().get(key).showInfo();
-        }
+        // load Treatment Locations
+        dbCom.loadTreatmentLocations();
+
+        // load Patients and their close contacts
+        dbCom.loadPatients();
+
+        // load Histories
+        dbCom.loadHistories();
+
+        // load Products and Packages
+        dbCom.loadProductsAndPackages();
+
+        // load Orders
+        dbCom.loadOrders();
+
+        ((Patient) app.getUserList().get("patient1")).payBill();
+
+//        for (String key : app.getUserList().keySet()) {
+//            app.getUserList().get(key).showInfo();
+//        }
     }
 }
