8e810930d7a9d6ebf51b646dd896dcf806e7a972
vkhangpham <vkhangpham@gmail.com>
2022-05-03 17:44:12 +0700

    update controllers for adminn

--- src/main/java/pck/java/fe/admin/ManageManagerAccController.java
+++ src/main/java/pck/java/fe/admin/ManageManagerAccController.java
@@ -1,12 +1,24 @@
 package pck.java.fe.admin;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 import pck.java.Index;
+import pck.java.be.app.App;
+import pck.java.be.app.user.IUser;
+import pck.java.be.app.user.Manager;
+import pck.java.database.DatabaseCommunication;
+import pck.java.fe.utils.LineNumbersCellFactory;
 
-public class ManageManagerAccController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ManageManagerAccController implements Initializable {
     public Button logoutButton;
     public Button backButton;
     public TableView tableViewManager;
@@ -15,6 +27,52 @@ public class ManageManagerAccController {
     public TableColumn colName;
     public TableColumn colBtn;
     public Button addMngButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        DatabaseCommunication.getInstance().loadManagers();
+        colNO.setCellFactory(new LineNumbersCellFactory<>());
+        colUsername.setCellValueFactory(new PropertyValueFactory<Manager, String>("username"));
+        colName.setCellValueFactory(new PropertyValueFactory<Manager, String>("name"));
+        Callback<TableColumn<Manager, String>, TableCell<Manager, String>> cellFactory
+                =
+                new Callback<>() {
+                    @Override
+                    public TableCell call(final TableColumn<Manager, String> param) {
+                        final TableCell<Manager, String> cell = new TableCell<Manager, String>() {
+
+                            final Button btn = new Button("Khoá tài khoản");
+
+                            @Override
+                            public void updateItem(String item, boolean empty) {
+                                super.updateItem(item, empty);
+                                if (empty) {
+                                    setGraphic(null);
+                                    setText(null);
+                                } else {
+                                    btn.setOnAction(event -> {
+                                        System.out.println(getClass() + "btn ban manager click");
+                                    });
+                                    btn.getStyleClass().addAll("btn", "btn-danger");
+                                    setGraphic(btn);
+                                    setText(null);
+                                }
+                            }
+                        };
+                        return cell;
+                    }
+                };
+        colBtn.setCellFactory(cellFactory);
+        //DatabaseCommunication.getInstance().loadManagers();
+
+        for (String key : App.getInstance().getUserList().keySet()) {
+            IUser iu = App.getInstance().getUserList().get(key);
+
+            if (iu.getRole() == IUser.Role.MANAGER) {
+                tableViewManager.getItems().add((Manager) iu);
+            }
+        }
+    }
 
     public void onBackButtonCliked(ActionEvent actionEvent) throws Exception {
         if(actionEvent.getSource() == backButton) {
